e8b2e793ef711aa401698746114e222987ebd6a1
admin@licebase.org <admin@licebase.org>
2015-08-14 16:27:45 +0200

    14.08.15

--- theme/geneshop_cart.js
+++ theme/geneshop_cart.js
@@ -11,6 +11,24 @@
         .css("left", ($(window).width() - $('div.messages-ajax').outerWidth()) / 2 + $(window).scrollLeft() + "px")
         .delay(2000)
         .fadeOut(1000); // Tongta delete Qty form
+
+      var inputCountValue = $("input.qty", context);
+      inputCountValue.bind({
+        keypress: function(e) {
+          if (e.which != 13 && e.which != 8 && e.which !== 0 && (e.which < 48 || e.which > 57)) {
+            return false;
+          }
+        },
+        focus: function() {
+          this.title = this.value;
+          this.value = "";
+        },
+        blur: function() {
+          if (this.value === "") {
+            this.value = this.title;
+          }
+        }
+      });
     }
   };
 })(jQuery);
