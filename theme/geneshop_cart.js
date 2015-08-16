15b1c51b5abcbf29faef3b9586c05e8ee8dc217d
admin@licebase.org <admin@licebase.org>
2015-08-16 19:16:26 +0200

    follow Qty

--- theme/geneshop_cart.js
+++ theme/geneshop_cart.js
@@ -10,15 +10,16 @@
       $('div.messages-ajax')
         .css("left", ($(window).width() - $('div.messages-ajax').outerWidth()) / 2 + $(window).scrollLeft() + "px")
         .delay(2000)
-        .fadeOut(1000); // Tongta delete Qty form
+        .fadeOut(1000); // Tongta
 
-      var inputCountValue = $("input.qty", context);
+      var inputCountValue = $("input.mRNA", context);
+      var inputCountValue = $("input.CDs", context);
       inputCountValue.bind({
         keypress: function(e) {
           if (e.which != 13 && e.which != 8 && e.which !== 0 && (e.which < 48 || e.which > 57)) {
             return false;
           }
-        },
+	},
         focus: function() {
           this.title = this.value;
           this.value = "";
