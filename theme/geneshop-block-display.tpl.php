2402d23c8cca864c266d22d508b58a026aa88607
Wei Zhang <djang.wei@gmail.com>
2017-08-17 15:01:25 +0200

    make it work on php7

--- theme/geneshop-block-display.tpl.php
+++ theme/geneshop-block-display.tpl.php
@@ -15,18 +15,17 @@
 if ($not_empty): ?>
   <div>
   <?php print $basket_items_list; ?>
-  </div>
+  <?php print $basket_page_link; ?>  
+</div>
+
   <?php if ($total_sum): ?>
     <div>
     <?php print t('Total price: ') . $total_sum; ?>
     </div>
   <?php endif; ?>
-  <div>
-  <?php print $basket_page_link; ?>
-  </div>
 <?php
 else : ?>
-<div>
+<!--div>
 <?php print t('Your basket is empty'); ?>
- </div>
+ </div -->
  <?php endif;
